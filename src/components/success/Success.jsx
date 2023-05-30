fb87eb5eedb10ff0b7cc3b11ecf7d5fdd78e211b
Tchouanfe feze boris <borisfeze9@gmail.com>
2023-05-30 23:58:26 +0100

    success stories

--- src/components/success/Success.jsx
+++ src/components/success/Success.jsx
@@ -1,32 +1,164 @@
 import React from "react";
-import styles from "../success/success.module.css";
+import { RiDoubleQuotesL } from "react-icons/ri";
+import { AiOutlineMinus, AiFillYoutube } from "react-icons/ai";
 
 function Success() {
   return (
-    <div className={styles.success}>
-      <div className={styles.success__text}>
-        <div className={styles.success__text__title}>
-          <h1>Success Stories</h1>
-          <div className={styles.success__text__title__small}>
-            <p>
-              It was once a dream, now a reality. Hear from our 2022 batch
-              students.
-            </p>
+    <div className="bg-[#198754] w-full  pt-10  h-full   px-10 2xl:px-20 py-3 ">
+      <div className="text-center text-white">
+        <div className="text-4xl font-bold">Success Stories</div>
+        <div className="text-2xl  pt-2">
+          It was once a dream, now a reality. Hear from our 2021 batch students.
+        </div>
+      </div>
+
+      <div className=" flex justify-around items-center md:items-center flex-wrap mt-12 ">
+        <div className="card__wrapper w-[350px] flex flex-col flex-wrap bg-gray-100 rounded-sm justify-center items-center p-4 my-4">
+          <div className="w-[85px] h-[85px] rounded-full border-[1px] border-gray-400 flex items-center justify-center bg-white">
+            <div className="w-[80px] h-[80px] rounded-full   ">
+              <img
+                src="/images/profile.jpg"
+                alt=""
+                className="w-[100%] h-[100%] rounded-full"
+              />
+            </div>
+          </div>
+          <div className="w-[300px] pt-2 text-center text-lg flex">
+            <div>
+              <RiDoubleQuotesL />
+            </div>
+            <div>
+              {" "}
+              Sir, helped me unlock my potential. I improved my score in Physics
+              from 40 to 140.
+            </div>
+          </div>
+          <div className="text-[.7rem] text-gray-500 flex items-center">
+            <AiOutlineMinus />
+            Mohammed Suhail S from Palani
+          </div>
+          <div className="text-center pt-2">NEET 2021 Score</div>
+          <div className="text-center text-xl font-bold">650</div>
+          <div className="text-center pt-2">Joining</div>
+          <div className="text-center pt-2 font-bold">
+            Madras Medical College, Chennai
+          </div>
+          <div className="w-full flex justify-center items-center py-4">
+            <button className="btn bg-[#CC0000] btn-sm border-0 rounded-[5px] text-white ">
+              <AiFillYoutube className="me-1" />
+              Testimonial
+            </button>
           </div>
         </div>
-        <div className="card w-96 glass">
-          <figure>
-            <img
-              src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="car!"
-            />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Life hack</h2>
-            <p>How to park your car at your garage?</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Learn now!</button>
+        <div className="card__wrapper w-[350px] flex flex-col flex-wrap bg-gray-100 rounded-sm justify-center items-center p-4 my-4">
+          <div className="w-[85px] h-[85px] rounded-full border-[1px] border-gray-400 flex items-center justify-center bg-white">
+            <div className="w-[80px] h-[80px] rounded-full   ">
+              <img
+                src="/images/profile.jpg"
+                alt=""
+                className="w-[100%] h-[100%] rounded-full"
+              />
             </div>
+          </div>
+          <div className="w-[300px] pt-2 text-center text-lg flex">
+            <div>
+              <RiDoubleQuotesL />
+            </div>
+            <div>
+              {" "}
+              Sir, helped me unlock my potential. I improved my score in Physics
+              from 40 to 140.
+            </div>
+          </div>
+          <div className="text-[.7rem] text-gray-500 flex items-center">
+            <AiOutlineMinus />
+            Mohammed Suhail S from Palani
+          </div>
+          <div className="text-center pt-2">NEET 2021 Score</div>
+          <div className="text-center text-xl font-bold">650</div>
+          <div className="text-center pt-2">Joining</div>
+          <div className="text-center pt-2 font-bold">
+            Madras Medical College, Chennai
+          </div>
+          <div className="w-full flex justify-center items-center py-4">
+            <button className="btn bg-[#CC0000] btn-sm border-0 rounded-[5px] text-white ">
+              <AiFillYoutube className="me-1" />
+              Testimonial
+            </button>
+          </div>
+        </div>
+        <div className="card__wrapper w-[350px] flex flex-col flex-wrap bg-gray-100 rounded-sm justify-center items-center p-4 my-4">
+          <div className="w-[85px] h-[85px] rounded-full border-[1px] border-gray-400 flex items-center justify-center bg-white">
+            <div className="w-[80px] h-[80px] rounded-full   ">
+              <img
+                src="/images/profile.jpg"
+                alt=""
+                className="w-[100%] h-[100%] rounded-full"
+              />
+            </div>
+          </div>
+          <div className="w-[300px] pt-2 text-center text-lg flex">
+            <div>
+              <RiDoubleQuotesL />
+            </div>
+            <div>
+              {" "}
+              Sir, helped me unlock my potential. I improved my score in Physics
+              from 40 to 140.
+            </div>
+          </div>
+          <div className="text-[.7rem] text-gray-500 flex items-center">
+            <AiOutlineMinus />
+            Mohammed Suhail S from Palani
+          </div>
+          <div className="text-center pt-2">NEET 2021 Score</div>
+          <div className="text-center text-xl font-bold">650</div>
+          <div className="text-center pt-2">Joining</div>
+          <div className="text-center pt-2 font-bold">
+            Madras Medical College, Chennai
+          </div>
+          <div className="w-full flex justify-center items-center py-4">
+            <button className="btn bg-[#CC0000] btn-sm border-0 rounded-[5px] text-white ">
+              <AiFillYoutube className="me-1" />
+              Testimonial
+            </button>
+          </div>
+        </div>
+        <div className="card__wrapper w-[350px] flex flex-col flex-wrap bg-gray-100 rounded-sm justify-center items-center p-4 my-4">
+          <div className="w-[85px] h-[85px] rounded-full border-[1px] border-gray-400 flex items-center justify-center bg-white">
+            <div className="w-[80px] h-[80px] rounded-full   ">
+              <img
+                src="/images/profile.jpg"
+                alt=""
+                className="w-[100%] h-[100%] rounded-full"
+              />
+            </div>
+          </div>
+          <div className="w-[300px] pt-2 text-center text-lg flex">
+            <div>
+              <RiDoubleQuotesL />
+            </div>
+            <div>
+              {" "}
+              Sir, helped me unlock my potential. I improved my score in Physics
+              from 40 to 140.
+            </div>
+          </div>
+          <div className="text-[.7rem] text-gray-500 flex items-center">
+            <AiOutlineMinus />
+            Mohammed Suhail S from Palani
+          </div>
+          <div className="text-center pt-2">NEET 2021 Score</div>
+          <div className="text-center text-xl font-bold">650</div>
+          <div className="text-center pt-2">Joining</div>
+          <div className="text-center pt-2 font-bold">
+            Madras Medical College, Chennai
+          </div>
+          <div className="w-full flex justify-center items-center py-4">
+            <button className="btn bg-[#CC0000] btn-sm border-0 rounded-[5px] text-white ">
+              <AiFillYoutube className="me-1" />
+              Testimonial
+            </button>
           </div>
         </div>
       </div>
