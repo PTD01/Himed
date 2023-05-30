50b95ca573f9bcfac6e01d519105a5a1de380900
Tchouanfe feze boris <borisfeze9@gmail.com>
2023-05-31 00:05:15 +0100

    success stories

--- src/components/success/Success.jsx
+++ src/components/success/Success.jsx
@@ -124,19 +124,41 @@ function Success() {
             </button>
           </div>
         </div>
-        <div className="card w-96 glass">
-          <figure>
-            <img
-              src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="car!"
-            />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Life hack</h2>
-            <p>How to park your car at your garage?</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Learn now!</button>
+        <div className="card__wrapper w-[350px] flex flex-col flex-wrap bg-gray-100 rounded-sm justify-center items-center p-4 my-4">
+          <div className="w-[85px] h-[85px] rounded-full border-[1px] border-gray-400 flex items-center justify-center bg-white">
+            <div className="w-[80px] h-[80px] rounded-full   ">
+              <img
+                src="/images/profile.jpg"
+                alt=""
+                className="w-[100%] h-[100%] rounded-full"
+              />
             </div>
+          </div>
+          <div className="w-[300px] pt-2 text-center text-lg flex">
+            <div>
+              <RiDoubleQuotesL />
+            </div>
+            <div>
+              {" "}
+              Sir, helped me unlock my potential. I improved my score in Physics
+              from 40 to 140.
+            </div>
+          </div>
+          <div className="text-[.7rem] text-gray-500 flex items-center">
+            <AiOutlineMinus />
+            Mohammed Suhail S from Palani
+          </div>
+          <div className="text-center pt-2">NEET 2021 Score</div>
+          <div className="text-center text-xl font-bold">650</div>
+          <div className="text-center pt-2">Joining</div>
+          <div className="text-center pt-2 font-bold">
+            Madras Medical College, Chennai
+          </div>
+          <div className="w-full flex justify-center items-center py-4">
+            <button className="btn bg-[#CC0000] btn-sm border-0 rounded-[5px] text-white ">
+              <AiFillYoutube className="me-1" />
+              Testimonial
+            </button>
           </div>
         </div>
       </div>
